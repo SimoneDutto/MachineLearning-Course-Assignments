236e7e4d4b5aefc15a6c3cd26929120c65ab1119
SimoneDutto <simonedutto8@gmail.com>
2019-11-02 17:33:25 +0100

    Add scaling to features

--- k-nearest.py
+++ k-nearest.py
@@ -13,8 +13,8 @@ print(y.shape)
 
 X = data[:,:2]
 
-X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.3, random_state=37)
-X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.28, random_state=37)
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.28, random_state=1)
 
 cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
 cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
@@ -23,20 +23,24 @@ accuracy=[]
 
 k_values = [1,3,5,7]
 
+max = 0
+
+
 for n_neighbors in k_values:
 
     knn = neighbors.KNeighborsClassifier(n_neighbors)
-    knn.fit(X_train, y_train)
+    
     scaler = StandardScaler()
     scaler.fit(X_train)
 
-    scaler.transform(X_train) # scale train set
-    scaler.transform(X_val) # scale validation set with the same parameters
+    X_train_scaled = scaler.transform(X_train) # scale train set
+    X_val_scaled = scaler.transform(X_val) # scale validation set with the same parameters
 
+    knn.fit(X_train_scaled, y_train)
     # Plot the decision boundary. For that, we will assign a color to each
     # point in the mesh [x_min, x_max]x[y_min, y_max].
-    x_min, x_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
-    y_min, y_max = X_train[:, 1].min() - 1, X_train[:, 1].max() + 1
+    x_min, x_max = X_train_scaled[:, 0].min() - 1, X_train_scaled[:, 0].max() + 1
+    y_min, y_max = X_train_scaled[:, 1].min() - 1, X_train_scaled[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.2),
                          np.arange(y_min, y_max, 0.2))
     
@@ -48,16 +52,22 @@ for n_neighbors in k_values:
     plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
 
     # Plot also the training points
-    plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cmap_bold,
+    plt.scatter(X_train_scaled[:, 0], X_train_scaled[:, 1], c=y_train, cmap=cmap_bold,
                 edgecolor='k', s=20)
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
-    plt.title("3-Class classification (k = %i)"
+    plt.title("Wine classification (k = %i)"
               % (n_neighbors))
 
-    y_pred = knn.predict(X_val)
-
-    accuracy.append(metrics.accuracy_score(y_val, y_pred))
+    y_pred = knn.predict(X_val_scaled)
+    acc = metrics.accuracy_score(y_val, y_pred)
+    
+    if(acc > max):
+        best_k = n_neighbors
+        max = acc
+        best_model = knn
+        best_scaler = scaler
+    accuracy.append(acc)
 
 plt.figure()
 # naming the x axis 
@@ -73,16 +83,10 @@ plt.title('Accuracy over k')
 # function to show the plot 
 plt.show() 
 
-k = input("Choose the K which performs better: ")
+print("Best model with k = "+str(best_k)+"and accuracy %f on validation set", max)
 
-scaler = StandardScaler()
+X_test_scaled = best_scaler.transform(X_test)
 
-scaler.fit_transform(X_train_val)
+y_pred_final = best_model.predict(X_test_scaled)
 
-scaler.transform(X_test)
-
-knn_final = neighbors.KNeighborsClassifier(k)
-
-y_pred_final = knn.predict(X_test)
-
-print(metrics.accuracy_score(y_test,y_pred_final))
+print("Accuracy: "+str(metrics.accuracy_score(y_test,y_pred_final)))
